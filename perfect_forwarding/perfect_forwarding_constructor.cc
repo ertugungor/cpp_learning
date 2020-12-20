df9ff41f454a373d0fadb0a1d7f53a8aa089d5b6
Ertugrul Gungor <ertugungor@gmail.com>
2020-12-20 17:33:17 +0300

    Improve code to show differences between rvalue references and universal references.

---
+++ perfect_forwarding/perfect_forwarding_constructor.cc
@@ -0,0 +1,20 @@
+#include <string>
+
+/**
+ * TODO
+ * 
+ * 1- basic constructor example working
+ * 2- Corner cases of overload matchings, copy constructors etc.
+ * 3- perfect forwarding constructor solution
+ * 4- alternatives (pass by value, const T& etc.) and their drawbacks
+ * 5- When perfect forwarding does not work
+ */
+
+class Person{
+
+public:
+  
+  
+private:
+  std::string name_;
+};
