c06b8016734bd134851ad63ab171568acc3b8efd
Ertugrul Gungor <ertugungor@gmail.com>
2021-01-24 03:16:15 +0300

    Improve perfect forwarding file

--- forwarding_references/forwarding.cc
+++ forwarding_references/forwarding.cc
@@ -29,30 +29,130 @@
  * 
  * 2- no runtime operation
  * 
+ * 
+ * Compile:
+ * 
+ * g++ -std=c++11 -o forwarding.out forwarding.cc
+ * 
  */
 
 #include <utility>
 #include <iostream>
 #include <string>
 
-void func(std::string& param){
-  std::cout << "func is initialized with std::string&" << std::endl;
+/**
+ * if some_func needs the parameters passed by reference
+ * it does not work. Copies will be given to some_func
+ */
+template<typename T>
+void bad_forwarder_1(T t) {
+  some_func(t);
 }
 
-void func(std::string&& param){
+/**
+ * Making reference solves previous problem. However we know that
+ * non-const lvalue references cannot be bound to rvalues. Thus, following
+ * lines do not compile
+ * 
+ * bad_forwarder_2(1.5f);
+ * bad_forwarder_2(ReturnInt());
+ * 
+ */
+template<typename T>
+void bad_forwarder_2(T& t) {
+  some_func(t);
+}
+
+/**
+ * Let's make it const then. Nope, what if some_func wants to
+ * take it's parameter by reference and change param's value?
+ * 
+ */
+template<typename T>
+void bad_forwarder_3(const T& t) {
+  some_func(t);
+}
+
+/**
+ * Solution is to override for different parameter types then.
+ */
+template<typename T> void bad_forwarder_4(const T& t) { some_func(t); }
+template<typename T> void bad_forwarder_5(T& t) { some_func(t); }
+
+/**
+ * What if some_func takes more than one parameter?
+ * 
+ * This is an exponential problem 2-parameter func results 4 overloads 
+ */
+template<typename T, typename K> void bad_forwarder_6(T& t, K& k) { some_func(t, k); }
+template<typename T, typename K> void bad_forwarder_7(const T& t, K& k) { some_func(t, k); }
+template<typename T, typename K> void bad_forwarder_7(T& t, const K& k) { some_func(t, k); }
+template<typename T, typename K> void bad_forwarder_7(const T& t, const K& k) { some_func(t, k); }
+
+/**
+ * What if we also want to optimize the forwarder and try to elide copies 
+ * made for rvalue params? That leads us to rvalue reference taking overloads
+ * 
+ * Things get worse and worse..
+ *
+ * Therefore we need some way to forward arbitrary number of function params
+ * to original function by preserving value type and cv-qualifiers.
+ * 
+ * Solution is perfect forwarding. 
+ */
+
+void func(std::string& param) {
+  std::cout << "func is initialized with std::string&" << std::endl;
+  std::cout << "param is: " << param << std::endl;
+}
+
+void func(std::string&& param) {
   std::cout << "func is initialized with std::string&&" << std::endl;
+  std::cout << "param is: " << param << std::endl;
 }
 
 template<typename T>
-void f(T&& param){
+void perfect_forwarder(T&& param) {
   // perfect forwarding
   func(std::forward<T>(param));
 }
 
+class Person {
+public:
+  Person(int id, const std::string& name) : id_{id}, name_{name} {
+    std::cout << "const std::string& name" << std::endl;
+    std::cout << "name is: " << name_ << std::endl;
+  }
+  Person(int id, std::string&& name) : id_{id}, name_{std::move(name)} {
+    std::cout << "std::string&& name" << std::endl;
+    std::cout << "name is: " << name_ << std::endl;
+  }
+private:
+  int id_;
+  std::string name_;
+};
+
+
+template<typename... Args> 
+void perfect_forwarder_variadic(Args&&... param) {
+  Person person{std::forward<Args>(param)...};
+}
+
+void PerfectForwarding() {
+  std::string song_name {"The Rains of Castemere"};
+  perfect_forwarder(song_name);
+
+  perfect_forwarder("Hands of Gold");
+}
+
+void PerfectForwardingVariadic() {
+  // perfect_forwarder_variadic(1, "The Song of the Seven"); does not work as we expect
+  perfect_forwarder_variadic(63, std::string{"The Song of the Seven"});
+
+  std::string song_name{"Rat Cook"};
+  perfect_forwarder_variadic(62, song_name);
+}
 
 int main(){
-  std::string song_name {"The Rains of Castemere"};
-  f(song_name);
-
-  f("Hands of Gold");
+  PerfectForwardingVariadic();
 } 
